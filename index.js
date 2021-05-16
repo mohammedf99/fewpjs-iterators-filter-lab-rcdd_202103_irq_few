43123e0e16126aff2b5167d51b265a443acc7b48
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-05-16 21:09:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function findMatching(arrOfDrivers, name) {
 
 function fuzzyMatch(arrOfDrivers, letter) {
   
-  return arrOfDrivers.filter(driver => driver.charAt(0).toLowerCase() === letter.toLowerCase());
+  return arrOfDrivers.filter(driver => driver.charAt(0));
   
 }
 
