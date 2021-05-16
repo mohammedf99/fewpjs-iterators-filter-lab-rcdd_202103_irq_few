f12076d34734143a1480e6c851223bb2b58b2370
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-05-16 21:10:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+
+function findMatching(arrOfDrivers, name) {
+  
+  return arrOfDrivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  
+}
+
+function fuzzyMatch(arrOfDrivers, letter) {
+  
+  return arrOfDrivers.filter(driver => driver.charAt(0) === letter.charAt(0));
+  
+}
+
+function matchName(arrOfDrivers, name) {
+  
+  return arrOfDrivers.filter(driver => driver.name === name);
+}
