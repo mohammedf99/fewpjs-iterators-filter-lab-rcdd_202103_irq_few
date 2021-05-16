e98ddf5883940dc745d4d3fb8e877f2f52f12f2a
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-05-16 21:03:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 
 function findMatching(arrOfDrivers, string) {
   
-  return arrOfDrivers.toLowerCase().filter(driver => driver === string.toLowerCase());
+  return arrOfDrivers.filter(driver => driver.toLowerCase() === string.toLowerCase());
   
 }
